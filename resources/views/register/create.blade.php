6bf59dc2c771c3a674c40b3d82807ed06125a858
chanbzz <cboswell1024@gmail.com>
2021-11-03 10:52:16 -0400

    barebone form created

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,31 @@
+
+<section>
+    <h1> Hey Girl </h1>
+    <form action="/register" method="POST"></form>
+        <label for="name">
+            NAME
+        </label>
+        <input type="text">
+    <form method='POST' action='/register'>
+        <label for='username'>
+            USERNAME
+        </label>
+        <input type='text'> 
+    <form action="/register" method="POST"></form>
+        <label for="password">
+            PASSWORD
+        </label>
+        <input type="password">
+    
+        <button type='submit'>
+            BUTTON 
+        </button>
+    
+        
+
+
+    
+
+
+</section>
+
